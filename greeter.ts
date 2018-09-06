57f04b675b3171902ed484ae3ad82f06d2f19e50
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-09-05 19:49:38 -0700

    woah classes are crazy in ts

--- greeter.ts
+++ greeter.ts
@@ -3,13 +3,22 @@ interface Person {
   lastName: string;
 }
 
+class Student {
+  fullName: string;
+  constructor(
+    public firstName: string,
+    public middleInitial: string,
+    public lastName: string
+  ) {
+    this.fullName = `${firstName} ${middleInitial}. ${lastName}`;
+    }
+}
+
 function greeter(person: Person) {
   return `Hello, ${person.firstName} ${person.lastName}!`;
 }
 
-let user = {
-  firstName: 'Not',
-  lastName: 'Henry'
-};
+let user = new Student ('Henry', 'J.', 'Oliveira');
+
 
 document.body.innerHTML = `<h1> ${greeter(user)} </h1>`;
